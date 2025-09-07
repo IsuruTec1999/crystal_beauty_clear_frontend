a09d271345e934a18616a141703ca08c611cdc4c
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-07 21:30:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ProductCard from './components/product_card'
+
+function App() {
+
+
+  return (
+    <>
+      <ProductCard name="Iphone" description="wsefszsdfrgsdfg" price="1000"/>
+      <ProductCard />
+      <ProductCard />
+    </>
+  )
+}
+  
+export default App
---
+++ src/components/product_card.jsx
@@ -0,0 +1,13 @@
+export default function ProductCard(props){
+    console.log(props)
+
+    return(
+            <div>
+                <h1>Product Name</h1>
+                <p>Product Description </p>
+                <p>Product Price</p>
+                <button>Add to cart</button>
+            </div>
+
+    )
+}
