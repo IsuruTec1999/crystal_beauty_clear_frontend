6957af11dd788a43e7c44df7c461a8488c0af216
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-15 10:04:22 +0530

    second_commit

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,16 @@
 export default function LoginPage() {
     return (
-        <div className="w-full bg-red-900 h-screen bg-[url(/login_bg.jpg)]">
-            <h1>Login Page</h1>
+        <div className="w-full bg-red-900 h-screen bg-[url(/login_bg.jpg)] bg-cover bg-center flex ">
+            <div className=" w-[50%] h-full">
+
+            </div>
+            <div className=" w-[50%] h-full flex justify-center items-center" >
+                <div className="w-[450px] h-[600px]  backdrop-blur-xl shadow-xl rounded-xl flex flex-col justify-center items-center">
+                    <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[10px]" type="Email" placeholder="Email"></input>
+                    <input className="w-[400px] h-[50px] border border-white rounded-xl text-center m-[10px]" type="Password" placeholder="Password"></input>
+                    <button className="w-[400px] h-[50px] bg-green-500 text-white  rounded-xl text-center m-[10px] cursor-pointer">Login</button>
+                </div>
+            </div>
         </div>
     );
 }
