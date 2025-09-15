20f652a817bd14fd7f655ed0c8484e183403be73
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-15 07:22:03 +0530

    second_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import './App.css'
 import Header from './components/header'
 import ProductCard from './components/product_card'
+import LoginPage from './pages/loginPage'
 
 function App() {
 
 
   return (
-    <div className='w-full h-screen bg-pink-200'>
-    <div className="w-[500px] flex flex-col justify-center items-center h-[500px] bg-gray-200 relative">
-      <div className='w-[90px] h-[90px] bg-red-500'>
-      </div>
-      <div className='w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] top-[50px]'>
-      </div>
-      <div className='w-[90px] h-[90px] bg-blue-500 fixed right-[50px] bottom-[50px]'>
-      </div>
-      <div className='w-[90px] h-[90px] bg-green-500'>
-      </div>
-    </div>
-    </div>
+   <LoginPage/>
   )
 }
   
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="w-full bg-red-900 h-screen bg-[url(/login_bg.jpg)]">
+            <h1>Login Page</h1>
+        </div>
+    );
+}
