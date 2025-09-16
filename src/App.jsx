d11af1c4cc249527bcba727e08fa8710acac6ea5
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-16 06:51:56 +0530

    first_commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Header from './components/header'
 import ProductCard from './components/product_card'
+import AdminPage from './pages/adminPage'
 import LoginPage from './pages/loginPage'
 
 function App() {
 
 
   return (
-   <LoginPage/>
+
+   <AdminPage/>
   )
 }
   
---
+++ src/pages/adminPage.jsx
@@ -0,0 +1,10 @@
+export default function AdminPage() {
+    return (
+        <div className="w-full h-screen bg-gray-200"> 
+           <div className="w-[300px] h-full bg-red-900">
+
+           </div>
+            
+        </div>
+    )
+}
