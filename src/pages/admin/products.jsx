ab3f38b31e670f2eef3680f58fb58fac504b073e
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-21 07:36:45 +0530

    product backend connect

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -1,7 +1,14 @@
+import axios from "axios"
+
 export default function AdminProductsPage() {
+
+axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product").then(
+    (response)=>{
+    console.log(response.data)
+})
     return (
-        <div className="w-full h-screen bg-red-900   "> 
+        <div className="w-full h-screen  rounded-lg "> 
             <h1>Products</h1>
         </div>
     )
-}
+} 
