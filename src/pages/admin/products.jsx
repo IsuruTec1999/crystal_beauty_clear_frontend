948e66b87d16153a7034a586f55d221c9fe2f158
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-21 08:18:10 +0530

    final commit

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -2,12 +2,12 @@ import axios from "axios"
 
 export default function AdminProductsPage() {
 
-    const [products, setProducts] = useState([]);
+    //const [products, setProducts] = useState([]);
 
 axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product").then(
     (response)=>{
     console.log(response.data)
-    setProducts(response.data)
+    //setProducts(response.data)
 })
     return (
         <div className="w-full h-screen  rounded-lg "> 
