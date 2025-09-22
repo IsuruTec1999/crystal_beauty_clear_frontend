47f2674978b251b622c0f862759ceaa164de50b4
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-22 06:57:17 +0530

    useEffect hooks used to first_time run on the page

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -1,14 +1,21 @@
 import axios from "axios"
+import { useEffect, useState } from "react";
 
 export default function AdminProductsPage() {
 
-    //const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    useEffect(
+        ()=>{
+            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product").then(
+            (response)=>{
+                console.log(response.data)
+                setProducts(response.data)
+        })
+        },
+        []
+    )
 
-axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product").then(
-    (response)=>{
-    console.log(response.data)
-    //setProducts(response.data)
-})
+
     return (
         <div className="w-full h-screen  rounded-lg "> 
             <h1>Products</h1>
