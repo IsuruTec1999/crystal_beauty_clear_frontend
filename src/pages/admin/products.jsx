610c41975285c05a8cef469cfa9b69a9a515496b
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-23 07:00:51 +0530

    product table creration

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa6";
 
 export default function AdminProductsPage() {
 
@@ -18,7 +19,10 @@ export default function AdminProductsPage() {
 
 
     return (
-        <div className="w-full h-screen  rounded-lg "> 
+        <div className="w-full h-screen  rounded-lg  relative"> 
+        <button className="text-white bg-gray-700 p-[12px] absolute bottom-5 right-5 rounded-full text-3xl cusor-pointer hover:bg-gray-300 hover:text-gray-700 ">
+            <FaPlus />
+        </button>
         <table className="w-full">
             <thead>
                 <tr>
@@ -36,7 +40,7 @@ export default function AdminProductsPage() {
                    (product,index)=>{
                     console.log("mapping"+product.productId)
                     return (
-                        <tr key={index} className="border-b-2 border-gray-400" >
+                        <tr key={index} className="border-b-2 border-gray-400 text-center cursor-pointer hover:bg-gray-700 hover:text-white" >
                             <td className="p-2">{product.productId}</td>
                             <td className="p-2">{product.name}</td>
                             <td className="p-2">{product.price}</td>
