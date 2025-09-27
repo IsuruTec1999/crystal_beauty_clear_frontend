c4bb92c3aa40b7a1d99bf05402b5ead16ddb5486
Isuru Dulaj <isurudulaj100@gmail.com>
2025-09-27 06:33:46 +0530

    create supabase and connect the client and supabase

--- src/pages/testing.jsx
+++ src/pages/testing.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react"
 
 
 export default function Testing() {
-    
+    const [file, setFile] = useState(null)
+    const supabase = createClient("https://qvypebqmubxswenpazbm.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF2eXBlYnFtdWJ4c3dlbnBhemJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NzY3NTUsImV4cCI6MjA3NDM1Mjc1NX0.IiEBUgbXcD4wb_wHyJU9Vsp3gzfneK-hnXmSCLSPGCg")
+
+    function handleUpload(){
+
+    }
 
     return (
         <div className="w-full h-screen  flex flex-col justify-center items-center">
             <input type="file" onChange={
                 (e) => {
-                   console.log (e.target.files)
+                   setFile (e.target.files[0])
             }
             }></input>
+            <button onClick={handleUpload} className="bg-blue-500 text-white px-4 py-2 rounded mt-4">Upload</button>
         </div>
     )
 }
