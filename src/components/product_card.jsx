61b5dbeb4f1ab0edcc7e4687c2a9b2886ebf0020
Isuru Dulaj <isurudulaj100@gmail.com>
2025-10-02 07:07:05 +0530

    product card modify

--- src/components/product_card.jsx
+++ src/components/product_card.jsx
@@ -5,8 +5,14 @@ export default function ProductCard(props){
     const product = props.product;
 
     return(
-            <Link className="w-[250px] m-4 h-[350px] bg-amber-200 border" >
-                <img src= {product.images[0]}/>
+            <Link className="w-[250px] m-4 h-[350px]  shadow-2xl " >
+                <img className="w-full h-[220px] object-cover"src= {product.images[0]}/>
+                <div className="h-[110px] w-full flex flex-col justify-center px-4"> 
+                    <p className="text-gray-400">{product.productId}</p>
+                    <p className="text-lg font-bold">{product.name}</p>
+                    <p className="text-lg text-pink-400">{product.price.toFixed(2)} <span className="line-through text-gray-400 text-sm">{product.price<product.labeledPrice && product.labeledPrice.toFixed(2)}</span></p>
+
+                </div>
             </Link>
 
     )
