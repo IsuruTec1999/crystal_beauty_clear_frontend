25121c4a8cc9dc5be53c65947eb6377387782cb3
Isuru Dulaj <isurudulaj100@gmail.com>
2025-10-02 08:20:26 +0530

    product overview page create & setup

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -1,5 +1,57 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useParams } from "react-router-dom"
+import Loader from "../../components/loader";
+
 export default function ProductOverview(){
+    const params = useParams();
+    console.log(params.id);
+    if(params.id == null){
+
+        window.location.href = "/products";
+    }
+
+
+    const [product, setProduct] = useState(null);
+    const [status, setStatus] = useState("loading");
+
+    useEffect(()=>{
+        console.log(import.meta.env.VITE_BACKEND_URL+"/api/product/"+params.id);
+        if(status == "loading" ){
+            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product/"+params.id).then(
+                (res)=>{
+                    console.log(res)
+                    setProduct(res.data);
+                    setStatus("loaded");
+                }
+            ).catch(
+                (err)=>{
+                    toast.error("Product Is Not Available");
+                    setStatus("error");
+                }
+            )
+        }
+
+    },[status]
+)
+
     return(
-        <div>ProductOverview</div>
+        <div className="w-full h-full">
+            {
+                status == "loading" && <Loader/>
+            }
+            {
+                status == "loaded" && 
+                    <div className="w-full h-full">
+                        Product Loaded
+                    </div>
+            }
+            {
+                status== "error" && <div>
+                    Error
+                    </div>
+            }
+        </div>
     )
 }
