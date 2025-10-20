a218702aead9eba749bb7a7ffe82186d1b32dd8d
Isuru Dulaj <isurudulaj100@gmail.com>
2025-10-20 08:11:36 +0530

    location update

--- src/pages/client/ContactUs.jsx
+++ src/pages/client/ContactUs.jsx
@@ -216,7 +216,7 @@ export default function ContactUs() {
         <div className="mt-16 bg-white rounded-3xl shadow-lg overflow-hidden">
           <div className="h-96">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830869428!2d-74.119763973046!3d40.69766374874431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2s!4v1621436441456!5m2!1sen!2s"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126220.05292978889!2d79.97196553395038!3d6.927079166507323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259c5d13cc171%3A0x9c76e7b2b1c0d7ff!2sColombo%2C%20Sri%20Lanka!5e0!3m2!1sen!2s!4v1625724854013!5m2!1sen!2s"
               width="100%"
               height="100%"
               style={{ border: 0 }}
